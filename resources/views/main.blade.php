41afc104515abbc45ac5af4bd25957b39d092cf8
MARVIN\Marvin <marvinsmart9@gmail.com>
2018-04-28 06:03:22 +0100

    Replaced

---
+++ resources/views/main.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{app()->getLocale()}}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{env('APP_NAME')}}</title>
+            <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="root">
+
+            
+        </div>
+        <script type="text/javascript" src= {{ asset('js/app.js') }} ></script>
+    </body>
+</html>
